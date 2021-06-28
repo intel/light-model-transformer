54200fc47d5200fd91a942aed07709fcfc066a0c
Rafik Saliev <rafik.f.saliev@intel.com>
2021-06-28 12:00:00 +0200

    Remove BERT subdirectory

--- BERT/env.sh
+++
@@ -1,6 +0,0 @@
-CORES_PER_SOCK=`lscpu |grep "^Core(s) per socket:" |awk -F: '{print $2}' |awk '{$1=$1};1'`
-
-echo "Set OMP_NUM_THREADS="$CORES_PER_SOCK
-export OMP_NUM_THREADS=$CORES_PER_SOCK
-export KMP_BLOCKTIME=1
-export KMP_AFFINITY=granularity=fine,compact,1,0
--- BERT/tf_ops/compile.sh
+++
@@ -1,6 +0,0 @@
-PYTHON='python'
-TF_CFLAGS=`$PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'`
-TF_LFLAGS=`$PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))'`
-
-#icpc -std=c++11 -shared bert.cc -o bert.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -qopenmp -lmkl_rt -liomp5 -xCORE-AVX512
-g++ -std=c++11 -shared bert.cc -o bert.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -fopenmp -lmkl_rt -liomp5 -xCORE-AVX512
